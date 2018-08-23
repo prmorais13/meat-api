f1b3b1433b6fd2931ec8ad23ec938e0af412370c
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-08-22 21:34:03 -0300

    Substituindo um Documento via PUT

--- users/users.router.ts
+++ users/users.router.ts
@@ -30,6 +30,22 @@ class UsersRouter extends Router {
         next()
       })
     })
+
+    application.put('/users/:id', (req, res, next) => {
+      const options = { overwrite: true }
+      User.update({ _id: req.params.id }, req.body, options)
+        .exec()
+        .then(result => {
+          if (result.n) {
+            return User.findById(req.params.id)
+          } else {
+            res.send(404)
+          }
+        }).then(user => {
+          res.json(user)
+          return next()
+        })  
+    })
   }
 
 }
