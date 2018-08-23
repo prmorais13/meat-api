9d6db0ede65ef8db4eaa1de7a302b824be3ecd14
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-08-22 22:11:24 -0300

    Alterando um documento via PATCH

---
+++ server/merge-patch.parser.ts
@@ -0,0 +1,15 @@
+import * as restify from 'restify'
+
+const mpContentType = 'application/merge-patch+json'
+
+export const mergePatchBodyParser = (req: restify.Request, res: restify.Response, next) => {
+  if (req.getContentType() === mpContentType && req.method === 'PATCH') {
+    (<any>req).rawBody = req.body
+    try {
+      req.body = JSON.parse(req.body)
+    } catch (e) {
+      return next(new Error(`Invalid content: ${e.menssage}`))
+    }
+  }
+  return next()
+}
